8c803f68581610af22c7ceb574efe4015f04fba9
Tijmen van Egmond <tijmenvanegmond@gmail.com>
2023-03-31 15:01:28 +0200

    init

---
+++ src/bot.ts
@@ -0,0 +1,24 @@
+const CLIENT_ID = 12345
+const TOKEN = 'TOKEN'
+const { REST, Routes } = require('discord.js');
+
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
