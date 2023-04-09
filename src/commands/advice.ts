97a5ea7b5846c776e59aa326b0f81f74ef9d5113
Tijmen van Egmond <tijmenvanegmond@gmail.com>
2023-04-09 15:18:23 +0200

    Leave after advice

--- src/commands/advice.ts
+++ src/commands/advice.ts
@@ -1,7 +1,6 @@
 import { CommandInteraction, Client, ApplicationCommandType, GuildMember, Channel, VoiceChannel } from "discord.js";
 import { Command } from "./command";
 import { StreamType, createAudioPlayer, createAudioResource, joinVoiceChannel } from '@discordjs/voice';
-import { channel } from "diagnostics_channel";
 
 export const Advice: Command = {
     name: "advice",
@@ -11,11 +10,8 @@ export const Advice: Command = {
 
         const member = interaction?.member as GuildMember;
         if (!member?.voice?.channelId) {
-            return await interaction.followUp({ content: 'You need to enter a voice channel before use the command', ephemeral: true })
+            return await interaction.followUp({ content: 'Amid discord, we will find tranquility.', ephemeral: true })
         }
-
-        // const lounge = await guild.channels.fetch("1055169128945688648")
-        //member.voice.setChannel("1055169128945688648");
 
         const channel = member.voice.channel as VoiceChannel;
         const connection = joinVoiceChannel({
@@ -24,9 +20,11 @@ export const Advice: Command = {
             adapterCreator: channel.guild.voiceAdapterCreator,
         });
 
+        const voiceLineUri = "https://static.wikia.nocookie.net/overwatch_gamepedia/images/c/cd/Zenyatta_-_Amid_discord%2C_we_will_find_tranquility.ogg/revision/latest?cb=20220630044422"
+
         const audioPlayer = createAudioPlayer();
         const subscription = connection.subscribe(audioPlayer);
-        const resource = createAudioResource('https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3', {
+        const resource = createAudioResource(voiceLineUri, {
             inputType: StreamType.Arbitrary,
         });
 
@@ -35,7 +33,11 @@ export const Advice: Command = {
         // subscription could be undefined if the connection is destroyed!
         if (subscription) {
             // Unsubscribe after 5 seconds (stop playing audio on the voice connection)
-            setTimeout(() => subscription.unsubscribe(), 5_000);
+            setTimeout(() => {
+                subscription.unsubscribe()
+                connection.disconnect()
+                connection.destroy()
+            }, 5_000);
         }
 
         const content = "consider this..";
