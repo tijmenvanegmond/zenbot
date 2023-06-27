5557ab461c8ece8440a652b6d6561e9c09064474
Tijmen van Egmond <tijmenvanegmond@gmail.com>
2023-06-27 02:09:36 +0200

    Voicechannel listner only cares about one channel

--- src/listeners/onVoiceChannelUpdate.ts
+++ src/listeners/onVoiceChannelUpdate.ts
@@ -1,40 +1,37 @@
-import { Client, Events, GuildMember, VoiceBasedChannel } from "discord.js";
+import { Client, Events, GuildMember, VoiceBasedChannel, VoiceChannel, VoiceState } from "discord.js";
+
+const VOICE_CHANNEL_ID = "1120775680624443413";
 
 export default (client: Client): void => {
-    client.on(Events.VoiceStateUpdate, (oldState, newState) => {
-        var name = newState.member?.displayName;
-        if (newState.member?.user.bot) {
-            console.log(`ignoring bot user ${name}`);
+    client.on(Events.VoiceStateUpdate, (oldState: VoiceState, newState: VoiceState) => {
+        const member = newState.member;
+        const botUser = member?.user.bot;
+
+        if (botUser) {
+            console.log(`Ignoring bot user ${member?.displayName}`);
             return;
         }
-        if (newState.channelId === null) {
-            console.log(`user ${name} left channel ${oldState.channelId} : ${oldState.channel?.name}`);
-            if (oldState.channelId === "1120775680624443413") {
-                updateChannel(client, oldState.channelId, newState.member!)
-            }
+
+        console.log(`user ${member?.displayName} moved from ${oldState.channelId} to ${newState.channelId}  `);
+
+        if (newState.channelId === VOICE_CHANNEL_ID) {
+            //user joined
+            const channel = newState.channel!;
+            updateChannel(channel, newState.member!)
         }
-        else if (oldState.channelId === null) {
-            console.log(`user ${name} joined channel ${newState.channelId}  : ${newState.channel?.name}`);
-            if (newState.channelId === "1120775680624443413") {
-                updateChannel(client, newState.channelId, newState.member!)
-            }
+        else if (oldState.channelId === VOICE_CHANNEL_ID) {
+            //user left
+            const channel = oldState.channel!;
+            updateChannel(channel, newState.member!)
         }
         else {
-            console.log(`user ${name} moved channels ${oldState.channelId} ${newState.channelId}`);
-            if (newState.channelId === "1120775680624443413") {
-                updateChannel(client, newState.channelId, newState.member!)
-            }
-            if (oldState.channelId === "1120775680624443413") {
-                updateChannel(client, oldState.channelId, newState.member!)
-            }
+            console.log(`Ignoring channel change`)
         }
     });
-
 };
 
-async function updateChannel(client: Client, channelId: string, member: GuildMember, left = false) {
+async function updateChannel(channel: VoiceBasedChannel, member: GuildMember, left = false) {
 
-    let channel = await client.channels.fetch(channelId) as VoiceBasedChannel;
     let newChannelName = getNewChannelName(channel, member);
 
     if (channel.name == newChannelName) {
@@ -43,8 +40,8 @@ async function updateChannel(client: Client, channelId: string, member: GuildMem
     }
 
     try {
-        await channel.setName(newChannelName)
-        console.log(`Updating Channel ${channel.id}'s name ${newChannelName}`);
+        console.log(`Updating Channel ${channel.id}'s name to :"${newChannelName}"`);
+        await channel.setName(newChannelName);
     } catch (error) {
         console.log(`Failed to update Channel name`);
         console.log(error);
@@ -53,33 +50,92 @@ async function updateChannel(client: Client, channelId: string, member: GuildMem
 }
 
 function getNewChannelName(channel: VoiceBasedChannel, member: GuildMember) {
+    let possibleNames: string[] = [];
 
-    let members = channel.members.map(x => x)
-        .filter(x => !x.user.bot)
-        .sort((a, b) => compareStrings(a.user.username, b.user.username));
+    let memberNames = channel.members
+        .filter(member => !member.user.bot)
+        .map(member => member.displayName)
+        .sort((a, b) => compareStrings(a, b));
 
-    let numMembers = members.length;
+    //is Timbo there?
+    if (channel.members.some(m => m.user.id === "299595170767306752"))
+        possibleNames.push("Discord Jerkoff Session")
+
+    //wild cards
+    possibleNames.push("Pixel Purgatory");
+    possibleNames.push("Nerdvana Lounge");
+    possibleNames.push("The Geek Grotto");
+    possibleNames.push("Gaming Asylum");
+    possibleNames.push("The LAN Party Lair");
+    possibleNames.push("Rage Quit Retreat");
+    possibleNames.push("Level Up Lounge");
+
+    let numMembers = memberNames.length;
+    console.log(`There's ${numMembers} in the voice channel`);
     switch (numMembers) {
         case 0:
-            return 'Empty Lounge';
+            possibleNames.push('Empty Lounge');
+            possibleNames.push('Empty Lounge');
+            possibleNames.push('Empty Lounge');
+            possibleNames.push('Empty Lounge');
+            possibleNames.push('Empty Lounge');
+            possibleNames.push("404 Gamers Not Found");
+            break;
         case 1:
-            if (member.user.id == "299595170767306752") //timbo
-                return "Discord Jerkoff Session"
-
-            return "Lonerism Lounge"
+            possibleNames.push("Solo Sensei");
+            possibleNames.push("Lonerism Lounge");
+            possibleNames.push("Lone Warrior");
+            possibleNames.push("Sole Survivor");
+            possibleNames.push("One-Man Army");
+            possibleNames.push(`${memberNames[0]}'s Hole`)
+            break;
         case 2:
-            return "Fun for 2 Lounge"
+            possibleNames.push("Dynamic Duo");
+            possibleNames.push("Power Pair");
+            possibleNames.push("Dual Warriors");
+            possibleNames.push("Twin Titans");
+            possibleNames.push("Double Trouble");
+            possibleNames.push("Pair of Legends");
+            possibleNames.push("Dynamic Dorks");
+            possibleNames.push("The Dual Delinquents");
+            possibleNames.push(`${memberNames[0]} & ${memberNames[0]}'s Get Together`)
+            break;
         case 3:
-            return "A Happy Treesome"
+            possibleNames.push("Trio of Trolls");
+            possibleNames.push("Triple Threat");
+            possibleNames.push("Triforce Warriors");
+            possibleNames.push("Three Musketeers");
+            possibleNames.push("Tactical Trinity");
+            possibleNames.push("Triumphant Triad");
+            break;
         case 4:
-            return "The Four Horseman"
+            possibleNames.push("The Four Horseman");
+            possibleNames.push("Quad Squad");
+            possibleNames.push("Fantastic Four");
+            possibleNames.push("Elite Ensemble");
+            possibleNames.push("Quartet of Chaos");
+            possibleNames.push("The Fabulous Four")
+            break;
         case 5:
-            return "One Swell Overwatch Team"
+            possibleNames.push("Pentaforce");
+            possibleNames.push("Quintessential Warriors");
+            possibleNames.push("Fivefold Fury");
+            possibleNames.push("The Fabulous Five")
+            break;
         case 6:
-            return "Epic Gamer Lounge"
+            possibleNames.push("Team Hexagon");
+            possibleNames.push("Six-Pack Power");
+            possibleNames.push("Epic Gamer Lounge");
+            possibleNames.push("The Savage Six");
+            break;
         default:
-            return "Overcrowded"
+            possibleNames.push("Overcrowded");
+            possibleNames.push("The Grand Assembly");
+            possibleNames.push("Discord Overflow");
+            break;
     }
+
+    return possibleNames[Math.floor(Math.random() * possibleNames.length)] ?? 'Lounge';
 }
 
 function compareStrings(a: string, b: string) {
